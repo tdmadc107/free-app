be8894aa0b406f40884067daf56d8eb8a6a2d540
tdmadc107 <dangdat111@gmail.com>
2022-12-05 22:59:27 +0700

    init project react-redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter } from 'react-router-dom'
 
-const name = prompt("Enter your name:");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <h1>Hello World</h1>
-          <p>{name ? name : "My friend"}</p>
-        </div>
-      </header>
-    </div>
+    <BrowserRouter></BrowserRouter>
   );
 }
 
